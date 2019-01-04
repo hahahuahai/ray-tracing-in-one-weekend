ac3f8c6628d98ed4e937174e5e8def3ee8cc6574
huahai <279598616@qq.com>
2019-01-04 17:11:03 +0800

    Chapter6

--- RTinaweek/Chapter6/main.cpp
+++ RTinaweek/Chapter6/main.cpp
@@ -3,6 +3,8 @@
 #include "../Utilities/sphere.h"
 #include "../Utilities/hitable_list.h"
 #include "float.h"
+#include "../Utilities/camera.h"
+#include "../Utilities/drand48.h"
 
 using namespace std;
 
@@ -27,25 +29,31 @@ int main()
 {
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 	cout << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
+
+	camera cam;
+
 	for (int j = ny - 1; j >= 0; j--)
 		for (int i = 0; i < nx; i++)
 		{
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
+			vec3 col(0, 0, 0);
 
-			ray r(origin, lower_left_corner + horizontal * u + vertical * v);
+			for(int s = 0;s<ns;s++)
+			{
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world);
+			}
 
-			vec3 p = r.point_at_parameter(2.0);
-			vec3 col = color(r, world);
+			col /= float(ns);
 
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
